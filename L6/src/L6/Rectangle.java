c1f7b968b9d0b0e786980cab14d78f85e7ad0f58
ajou <ajou@ajou-PC>
2016-04-27 16:11:34 +0900

    f bgf

---
+++ L6/src/L6/Rectangle.java
@@ -0,0 +1,37 @@
+package L6;
+
+public abstract class Rectangle extends Shape{
+	private double width;
+	private double height;
+	public double getWidth(){
+		return width;
+	}
+	public void setWidth(double w){
+		width=w;
+	}
+	public double getheight(){
+		return height;
+	}
+	public void setheight(double h){
+		height=h;
+	}
+	public double calculateArea(){
+		return width*height;
+	}
+	public double calculatePerimeter(){
+		return 2*(width+height);
+	}
+	public void drawRectangle(){
+		for(int a=0;a<height;a++){
+			for(int b=0;b<width;b++){
+				System.out.print("คั");
+			}
+		}
+	}
+	public void printInfo(){
+		System.out.println("width : "+width);
+		System.out.println("height : "+height);
+		System.out.println("area : "+calculateArea());
+		System.out.println("perimeter : "+calculatePerimeter());
+	}
+}
