b34a642d26f32040002d3931dcd041d011bf0d01
ajou <ajou@ajou-PC>
2016-04-27 16:53:02 +0900

    new classes

---
+++ L6/src/L6/Circle.java
@@ -0,0 +1,30 @@
+package L6;
+
+public class Circle extends Shape {
+		double radius;
+		final double PI = 3.14;
+		
+		public Circle(double radius){
+			this.radius = radius;
+		}
+		
+		public double getRadius() {
+			return radius;
+		}
+
+		public void setRadius(double radius) {
+			this.radius = radius;
+		}
+
+		public double calculateArea(){
+			return PI*radius*radius;
+		}
+		
+		public double calculatePerimeter(){
+			return 2*PI*radius;
+		}
+		
+		public void printInfo(){
+			
+		}
+}
