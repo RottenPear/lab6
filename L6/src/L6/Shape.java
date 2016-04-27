9e5c0ff5ac2511ff37a319e095ff695f19750030
Bae Hyunmo <hyunmo27@gmail.com>
2016-04-27 15:56:39 +0900

    complete Shaped

--- L6/src/L6/Shape.java
+++ L6/src/L6/Shape.java
@@ -1,7 +1,15 @@
 package L6;
 
 public abstract class Shape {
+	private int id;
 
-	
-	private int ID;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	abstract double calculateArea();
+	abstract double calculatePerimeter();
+	abstract void printInfo();
 }
